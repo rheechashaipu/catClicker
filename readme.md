1d3cd5b9f9d632861ff8fe2ca251976e1930dd86
R <rheechashaipu@gmail.com>
2016-07-05 20:03:49 -0400

    Added images, admin mode, and MVC setup in the js file.
    Much more could be done to improve the project such as getting the page to instantly
    render changes submitted in the admin input fields.

---
+++ readme.md
@@ -0,0 +1,16 @@
+Disclaimer: The MVC setup in script.js borrows heavily from the solution code. 
+
+The added on "admin" feature is my creation alone.
+
+To use, open the index.html file and select a cat from the cat list (above the picture of the cat). Clicking on the cat picture will increment
+the click counter (shown below the cat picture) by one click. 
+
+You may change the name of the cat, set a new click count (use integers), and even change the 
+cat picture. To do this, click the "Admin Mode" button to open up a set of input fields in which you may make the desired changes. To save and apply these changes, click the "Save Changes" button. To exit the admin features and clear the input fields, click "Cancel and Close Admin."
+
+This project was done as an exercise on MVC as well as practicing Javascript and DOM interactions. Please be kind, I'm still a noob at all this.
+
+
+Thanks,
+
+Richard
--- script.js
+++ script.js
@@ -1,17 +1,180 @@
-var cat1 = document.getElementById('cat1');
-counter1=0;
-counter2=0;
+//Stores cat data
+
+var model = {
+	currentCat: null,
+	//Making a list of key value pairs in this object
+	cats: [{
+		clickCount: 0,
+		catName: 'cat1',
+		imgSrc: 'images/cat1.jpg'
+	},
+	{
+		clickCount: 0,
+		catName: 'cat2',
+		imgSrc: 'images/cat2.jpg'
+	},
+	{
+		clickCount: 0,
+		catName: 'cat3',
+		imgSrc: 'images/cat3.jpg'
+	},
+	{
+		clickCount: 0,
+		catName: 'cat4',
+		imgSrc: 'images/cat4.jpg'
+	},
+	{
+		clickCount: 0,
+		catName: 'cat5',
+		imgSrc: 'images/cat4.jpg'
+	}
+	]
+};
 
 
-cat1.addEventListener('click', function(){
-  //the element has been clicked... do stuff here
-  counter1 +=1;
-  document.getElementById("cat1Counter").innerHTML = "You clicked gray cat "+counter1+" times.";
-}, false);
+//Controller, model and view do not directly communicate
+var octopus = {
 
-var cat2 = document.getElementById('cat2');
+	//initializes the first cat, initializes the interface for the first cat
+	init: function(){
+		model.currentCat = model.cats[0];
+		catPicView.init();
+		catButtonView.init();
+		adminView.init();
+	},
 
-cat2.addEventListener('click', function(){
-	counter2 +=1;
-	document.getElementById('cat2Counter').innerHTML = "You clicked creme cat "+counter2+" times.";
-}, false);
+	getCurrentCat: function(){
+		return model.currentCat;
+	},
+
+	getCats: function(){
+		return model.cats;
+	},
+
+	setCurrentCat: function(cat){
+		model.currentCat = cat;
+	},
+
+	incrementCounter: function(){
+		model.currentCat.clickCount++; //increment click
+		catPicView.render();
+	}
+
+}; 
+
+//View section
+
+var catPicView = {
+	//the init function here sets dom elements to variables.
+	init: function() {
+		this.catPicFrame = document.getElementById('catImg');
+		this.catName = document.getElementById('catName');
+		this.catClickCount = document.getElementById('catCounter');
+
+		this.catPicFrame.addEventListener('click', function(){
+			octopus.incrementCounter();
+		});
+
+		this.render();
+	},
+
+	render: function(){
+		var currentCat = octopus.getCurrentCat();
+		this.catPicFrame.src = currentCat.imgSrc;
+		this.catClickCount.textContent = "Current clicks: " + currentCat.clickCount;
+		this.catName.textContent = currentCat.catName;
+	}
+};
+
+var catButtonView = {
+
+	init: function() {
+		this.catListElem = document.getElementById('catList');
+
+		this.render();
+	},
+
+	render: function() {
+
+		//document.getElementById("catList").innerHTML = "";
+
+		var elem;
+
+		var cats = octopus.getCats();
+
+
+		for (var i=0; i<cats.length; i++){
+			cat = cats[i];
+
+			elem =document.createElement('li');
+			elem.textContent=cat.catName;
+
+			this.catListElem.appendChild(elem);
+
+			elem.addEventListener('click', (function(catcopy){
+				return function(){
+					octopus.setCurrentCat(catcopy);
+					catPicView.render();
+				};
+			})(cat));
+		}
+
+	}
+
+}
+
+var adminView = {
+	init: function() {
+		this.adminButton = document.getElementById('adminButton');
+		this.saveButton = document.getElementById('saveButton');
+		this.cancelButton = document.getElementById('cancelButton');
+
+		this.render();
+	},
+
+	render: function(){
+		document.getElementById('saveButton').style.display="none";
+		document.getElementById('cancelButton').style.display="none";
+		document.getElementById('catInfo').style.display="none";
+		//returns current cat data from model
+	
+		this.adminButton.addEventListener('click',function(){
+			var cat = octopus.getCurrentCat();
+		    var catAdmin = octopus.getCurrentCat();
+
+			//makes admin button display other admint buttons and input forms
+			document.getElementById("catInfo").style.display = "block";
+			saveButton.style.display="inline-block";
+			cancelButton.style.display="inline-block";
+
+			//sets initial admin values to current cat data
+			document.getElementById('newCatName').value = cat.catName;
+			document.getElementById('newImgSrc').value = cat.imgSrc;
+			document.getElementById('newClickCount').value = cat.clickCount;
+
+		})
+
+		this.saveButton.addEventListener('click', function(){
+			var cat = octopus.getCurrentCat();
+			var catAdmin = octopus.getCurrentCat();
+
+			var inputName = document.getElementById('newCatName').value;
+			cat.catName = inputName;
+			console.log(inputName);
+			var newClickCount = document.getElementById('newClickCount').value;
+			cat.clickCount = newClickCount;
+			var newImgSrc = document.getElementById('newImgSrc').value;
+			cat.imgSrc = newImgSrc;
+		})
+		this.cancelButton.addEventListener('click', function(){
+			document.getElementsByClassName("adminChanges").value = "";
+			catInfo.style.display = "none";
+			cancelButton.style.display="none";
+			saveButton.style.display="none";
+		})
+		}
+	};
+
+
+
+octopus.init();
